fc2a4d43ec15aa3a3320ab61b7a0e130b82746a6
Alejandro Rojas <ale402152@gmail.com>
2024-02-20 17:43:46 -0300

    normalizing styles

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import styleImport from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [
-    react(),
-    styleImport({
-      extensions: [".css", ".sass"],
-    }),
-  ],
+  plugins: [react()],
+  css: {
+    modules: true, // Habilita los módulos de CSS
+  },
 });
