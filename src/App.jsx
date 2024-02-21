933f4afbf65d66bf31d4c063cb812707630271b3
Alejandro Rojas <ale402152@gmail.com>
2024-02-21 18:03:24 -0300

    styling the navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "./styles/prueba.scss";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <div>
-        <h1>Hello world</h1>
+        <NavBar />
       </div>
     </>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,28 +3,30 @@ import styles from "../styles/NavBar.module.scss";
 
 const NavBar = () => {
   return (
-    <nav>
-      <div className={styles.navContainer}>
-        <img src="../../public/images/logo.svg" />
-        <ul>
-          <li>Collections</li>
-          <li>Man</li>
-          <li>Woman</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div className={styles.carUserContiner}>
-        <span className={styles.cart}>
-          <img src="../../public/images/icon-cart.svg" />
-          <span></span>
-        </span>
-        <img
-          src="../../public/images/image-avatar.png"
-          className={styles.userImage}
-        />
-      </div>
-    </nav>
+    <>
+      <nav className={styles.navBar}>
+        <div className={styles.navContainer}>
+          <img src="../../public/images/logo.svg" />
+          <div className={styles.navList}>
+            <span className={styles.listItem}>Collections</span>
+            <span className={styles.listItem}>Man</span>
+            <span className={styles.listItem}>Woman</span>
+            <span className={styles.listItem}>About</span>
+            <span className={styles.listItem}>Contact</span>
+          </div>
+        </div>
+        <div className={styles.cartUserContainer}>
+          <span className={styles.cart}>
+            <img src="../../public/images/icon-cart.svg" />
+            <span></span>
+          </span>
+          <img
+            src="../../public/images/image-avatar.png"
+            className={styles.userImage}
+          />
+        </div>
+      </nav>
+    </>
   );
 };
 
