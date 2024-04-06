2199997c39817321531bed25ac06b123162c5d4d
Alejandro Rojas <ale402152@gmail.com>
2024-04-06 04:27:21 -0300

    build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  base: "https://alejandrox64.github.io/ecommerce-product",
   css: {
     modules: true, // Habilita los módulos de CSS
   },
