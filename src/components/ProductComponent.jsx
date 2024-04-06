e7e1f34ee5a7e41fa254dac6303324ab224bc5be
Alejandro Rojas <ale402152@gmail.com>
2024-04-06 16:05:40 -0300

    change routes

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -115,7 +115,7 @@ const ProductComponent = () => {
             onClick={(e) => {
               handleMainImageClick(e);
             }}
-            src={`../../public/images/image-product-${indexImage}.jpg`}
+            src={`c:/Users/Alejandro/Desktop/Proyectos/frontEndMentor/ecommerceProductPageMain/ecommerce/public/images/image-product-${indexImage}.jpg`}
             id={indexImage}
             className={`${styles.shownImage}`}
             alt="shoes"
