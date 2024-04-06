d5d86715975d286b0728766c2581d5bf5814cebf
Alejandro Rojas <ale402152@gmail.com>
2024-04-06 04:38:08 -0300

    fixed error of route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import ProductComponent from "./components/ProductComponent";
-import NavBar from "c:/Users/Alejandro/Desktop/Proyectos/frontEndMentor/ecommerceProductPageMain/ecommerceProductPageMain/ecommerce/src/components/NavBar";
+import NavBar from "c:/Users/Alejandro/Desktop/Proyectos/frontEndMentor/ecommerceProductPageMain/ecommerce/src/components/NavBar";
 
 function App() {
   return (
